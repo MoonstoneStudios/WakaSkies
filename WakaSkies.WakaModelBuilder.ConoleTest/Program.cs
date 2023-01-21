ca590fb0d3178f3a02c83f7bb394034257462efb
Moonstone Studios <owenjh12345@gmail.com>
2023-01-20 17:49:32 -0800

    Added STL serializer + tests

---
+++ WakaSkies.WakaModelBuilder.ConoleTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using WakaSkies.WakaAPI;
+using static System.Console;
+
+namespace WakaSkies.WakaModelBuilder.ConoleTest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // using static Console.
+            WriteLine("WakaAPI tests.");
+            WriteLine("Please type in a username.");
+            string userName = ReadLine();
+            WriteLine("Please type in an API key.");
+            string key = ReadLine();
+            WriteLine("Please type in a year.");
+            string year = ReadLine();
+            WriteLine("Fetching...");
+
+            var waka = new WakaTime(key);
+            var response = await waka.GetUserInsights(userName, year);
+            if (response.Successful)
+            {
+                WriteLine("Type directory for stl file download.");
+                string directory = ReadLine();
+
+                ModelBuilder builder = new ModelBuilder();
+                if (Directory.Exists(directory))
+                {
+                    WakaModel model = builder.BuildModel(response);
+                    STLSerializer serializer = new STLSerializer();
+
+                    File.WriteAllText(directory + "/file.stl", serializer.Serialize(model));
+
+                    WriteLine($"Saved to {directory}\\file.stl");
+                }
+            }
+            else
+            {
+                WriteLine($"An error occured: {(int)response.ErrorData.StatusCode} {response.ErrorData.Reason}.");
+            }
+            WriteLine("Press any key to quit.");
+            ReadKey();
+        }
+    }
+}
